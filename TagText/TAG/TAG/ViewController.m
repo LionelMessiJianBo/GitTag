abefbcd99a4917182b98905e7754690cfe0342a5
bjb <1907386312@qq.com>
2017-08-15 09:24:37 +0800

    提交

---
+++ TagText/TAG/TAG/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  TAG
+//
+//  Created by pdid on 2017/8/15.
+//  Copyright © 2017年 pdid. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    NSString *_dev;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
